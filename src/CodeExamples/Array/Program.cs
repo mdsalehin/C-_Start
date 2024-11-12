468ec59efb6ccb47bd8553be76a33b84c6dfb383
mdsalehin <salahin1999@gmail.com>
2024-11-12 11:02:29 +0600

    Array Declear

---
+++ src/CodeExamples/Array/Program.cs
@@ -0,0 +1,21 @@
+﻿//Array
+
+//datatypes ar por [] declearing an array = new again datatypes [size]
+
+int[] x = new int[20];
+bool[] y = new bool[10];
+string[] s = new string[25];
+DateTime[] d = new DateTime[12];
+
+
+//Value assign in array
+
+x[0] = 1;
+x[1] = 2;
+x[2] = 3;
+
+//Another process Value assign in array
+char[] c = new char[] {'a','b','#'};
+char[] c = new char[3] { 'a', 'b', '#' };
+int[] z = new int[7] {1,2,3,4,5,9,10};
+int[] z = new int[] { 1, 2, 3, 4, 5, 9, 10 };

64da1818f2f6177863f4db2161c932acaf93cc2f
mdsalehin <salahin1999@gmail.com>
2024-11-12 10:44:39 +0600

    Variables primitive tyes

--- src/CodeExamples/Variables/Program.cs
+++ src/CodeExamples/Variables/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Datatypes of c# this is Primitive datatypes
+
+int x = 5;
+int y = 6;
+int sum = 0;
+sum = x + y;
+Console.WriteLine(sum);
+
+
+double d = 3.4;
+bool b = true;
+
+string s = "My name is salahin";
+char c = 'c'; // ascii character 0-255;
+
+//so constant value is not changeable it is fixed.
+const int minimum_age = 6;
+Console.WriteLine($"minimum_age: {minimum_age}");

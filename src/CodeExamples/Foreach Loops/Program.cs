1d28f73a89ea4c509f6951a4f39f445b9ea547a2
mdsalehin <salahin1999@gmail.com>
2024-11-13 14:28:21 +0600

    Foreach Loops use

---
+++ src/CodeExamples/Foreach Loops/Program.cs
@@ -0,0 +1,15 @@
+﻿
+/*
+    foreach loops ta use ta use korte hoi collection data type ar upore.
+    so array one kinds of collection of  datatypes so we can use it.
+ */
+
+//single dimention array declear .short cut syntext only work for single dimention array to be work.
+int[] ages = [30, 31, 18, 24, 25, 26];
+
+double sum = 0;
+foreach(int variablesStoreFromAges in ages)
+{
+    sum  = sum + variablesStoreFromAges;
+}
+Console.WriteLine(sum/ages.Length);

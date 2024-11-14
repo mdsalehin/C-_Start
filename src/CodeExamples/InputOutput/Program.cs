eccea87bf6e738f43325b4072bfd1b909804ea60
mdsalehin <salahin1999@gmail.com>
2024-11-14 12:18:34 +0600

    split,parse,foreach,for_loops,String mathos using(.sign),inputOutput

---
+++ src/CodeExamples/InputOutput/Program.cs
@@ -0,0 +1,49 @@
+﻿
+string line  = Console.ReadLine();
+
+string[] lineParts   = line.Split(',', ' ');
+
+for (int i = 0; i < lineParts.Length; i++)
+{
+    lineParts[i] = lineParts[i].Trim();
+}
+
+int[] numbers = new int[lineParts.Length];
+int count = 0;
+
+foreach(string varibale in lineParts)
+{
+    if(!string.IsNullOrWhiteSpace(varibale))
+    {
+        //string to int convert using parse mathodes
+        numbers[count++] = int.Parse(varibale);
+    }
+
+}
+/*for(int i = 0; i<lineParts.Length; i++)
+{
+    if (!string.IsNullOrWhiteSpace(lineParts[i]))
+    {
+        //string to int convert using parse mathodes
+        numbers[count++] = int.Parse(lineParts[i]);
+    }
+}*/
+
+for (int i = 0;i < count; i++)
+{
+    Console.WriteLine(numbers[i]);
+}
+//Alternative of for loops this will also work
+
+/*foreach(int var  in numbers.Take(count))
+{
+    Console.WriteLine(var);
+}*/
+
+int sum = 0;
+for (int i = 0; i<count; i++)
+{
+    sum = sum + numbers[i];
+}
+Console.Write("Sumation of the result = " +  sum);
+

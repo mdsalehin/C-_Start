fddd5cd212f71927ba38f9ed38d4f8616ec8a846
mdsalehin <salahin1999@gmail.com>
2024-11-14 20:36:50 +0600

    Char Mathods

---
+++ src/CodeExamples/charMathods/Program.cs
@@ -0,0 +1,26 @@
+﻿
+//Now char ar Mathod
+Console.WriteLine("char r Mathod usign .char sign and used");
+bool var = char.IsLetter('9');
+Console.WriteLine(var);
+
+
+
+/*Console.WriteLine("Enter a character input:");
+char input = (char)Console.Read();
+Console.WriteLine("Console print output only: " + char.IsLetterOrDigit(input));
+*/
+while (true)
+{
+ 
+    char input1 = (char)Console.Read();
+
+
+    if (input1 == '\r' || input1 == '\n')
+    {
+        break;
+    }
+    Console.WriteLine(char.IsLetterOrDigit(input1));
+}
+
+

b0eda2bc0c10d1aa5ad875f4236060fa2593b107
mdsalehin <salahin1999@gmail.com>
2024-11-15 03:12:45 +0600

    Encapsulation

---
+++ src/CodeExamples/Encapsulation/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+//Object Declare
+WatterBottle objectName = new WatterBottle("red", 100);
+
+
+//Another object declare
+WatterBottle objectAnother = new WatterBottle("black", 150);
+
+
+
+//Mathod Calling
+objectName.AddWater(30);
+objectAnother.AddWater(50);
+objectAnother.AddWater(30);
+
+
+//accessing property 
+Console.WriteLine(objectName.PropertyDeclare);
+Console.WriteLine(objectAnother.PropertyDeclare);
+
+Console.WriteLine(objectName.GetingCapacityProperty);
+Console.WriteLine(objectName.GetingColorProperty);
+
+Console.WriteLine(objectAnother.GetingColorProperty);
+Console.WriteLine(objectAnother.GetingCapacityProperty);
---
+++ src/CodeExamples/Encapsulation/WatterBottle.cs
@@ -0,0 +1,70 @@
+﻿class WatterBottle
+{
+    private double capacity;
+    private string color;
+    private double waterAmount;
+
+
+    public string GetingColorProperty
+    {
+        get
+        {
+            return color;
+        }
+    }
+
+    public double GetingCapacityProperty
+    {
+        get
+        {
+            return capacity;
+        }
+    }
+    public double PropertyDeclare
+    {
+        get
+        {
+            return waterAmount;
+        }
+       private set
+        {   
+
+            if (value <= capacity)
+            {
+                waterAmount = value;
+            }
+          
+        }
+    }
+
+
+    //constuctor  =  always calss ar name onujai e hoye thake
+    public WatterBottle(string constructorColor, double capacity)
+    {
+        color = constructorColor;
+        this.capacity = capacity; //same name use korle this use kore thaki.
+    }
+
+
+    public void AddWater(double amount)
+    {
+        if (PropertyDeclare + amount <= capacity)
+        {
+            PropertyDeclare = PropertyDeclare + amount;
+        }
+    }
+    public void ReleaseWater(double amount)
+    {
+        if (PropertyDeclare < amount)
+        {
+            PropertyDeclare = 0;
+        }
+        else
+        {
+            PropertyDeclare -= amount;
+        }
+
+    }
+}
+
+

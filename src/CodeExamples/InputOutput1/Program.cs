17d4cb73a3d8963574980e27ef934c1297bc818c
mdsalehin <salahin1999@gmail.com>
2024-11-14 13:54:12 +0600

    another input process.previously know how many number i have to input.

---
+++ src/CodeExamples/InputOutput1/Program.cs
@@ -0,0 +1,44 @@
+﻿
+//Another Input Process 
+void InputProcees2()
+{
+    //int count  = int.Parse(Console.ReadLine());
+
+    string varibale = Console.ReadLine();
+    int count = int.Parse(varibale);
+
+
+    int[] array = new int[count];
+    for (int i = 0; i < count; i++)
+    {
+        string numline = Console.ReadLine();
+        array[i] = int.Parse(numline);
+
+    }
+    int sum = 0;
+    /* for (int i = 0;i < count; i++)
+     {
+         sum = sum + array[i];
+     }*/
+
+    foreach (int variabless in array)
+    {
+        sum = sum + variabless;
+
+    }
+    Console.WriteLine(sum);
+}
+InputProcees2();
+//InputProcees2();
+
+
+
+/* sample input and output
+   5 
+  5 5 5 5 5 = 20 */
+
+/*
+ Why Parse Is Necessary:
+  The   Console.ReadLine()      method always returns a string regardless of what the user types, 
+  so to store the value as an int, you need to convert it using int.Parse().
+ */
